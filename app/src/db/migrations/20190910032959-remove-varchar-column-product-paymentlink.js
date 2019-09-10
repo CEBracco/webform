db89979f629763ff698bc94d0eef5e8b3dd6e2f1
CEBracco <bracco66.cb@gmail.com>
2019-09-10 02:04:48 -0300

    change type of column paymentlink

---
+++ app/src/db/migrations/20190910032959-remove-varchar-column-product-paymentlink.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Products', 'paymentLink');
+  },
+  
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn('Products', 'paymentLink', {
+      type: Sequelize.STRING,
+      after: "acceptedPhotos"
+    });
+  }
+};
