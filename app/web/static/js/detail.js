ee61bf6a4e06ac89030a46d9ee0d5dfee95c4310
cbracco <cbracco@cespi.unlp.edu.ar>
2019-09-12 15:42:59 -0300

    disable button on final send

--- app/web/static/js/detail.js
+++ app/web/static/js/detail.js
@@ -2,10 +2,14 @@ $(document).ready(function () {
     $('.btn-next').click(function () {
         var deliveryMethod = $("input[name='deliveryMethod']:checked").val();
         var paymentMethod = $("input[name='paymentMethod']:checked").val();
+        $('.btn-next').addClass('disabled');
         OrderService.setDeliveryAndPayment({ orderId: orderId, deliveryMethod: deliveryMethod, paymentMethod: paymentMethod }, function (res) {
             if (res.ok) {
                 window.location.href = res.data.completeUrl;
             }
+            setTimeout(function () { 
+                $('.btn-next').removeClass('disabled') 
+            }, 3000);
         })
     });
 
