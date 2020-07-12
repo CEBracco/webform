9b25e5d8542ee7517f6a63e512b813f9dae385b7
CEBracco <bracco66.cb@gmail.com>
2020-07-12 16:11:37 -0300

    fix backgrounds order

--- app/src/controllers/backgroundController.js
+++ app/src/controllers/backgroundController.js
@@ -7,8 +7,14 @@ global.server.app.get(['/background/:orderId'], function (req, res) {
             res.redirect('/');
         }
         db.Variation.findAll({ where: { type: 'background' } }).then(backgrounds => {
-            res.render("background", { backgrounds: backgrounds, order: order });
+            res.render("background", { backgrounds: orderBackgrounds(backgrounds), order: order });
             res.end();
         })
     });
 });
+
+function orderBackgrounds(backgrounds) {
+    return backgrounds.sort(function (b1, b2) {
+        return `${b1.name}0`.match(/\d+/)[0] - `${b2.name}0`.match(/\d+/)[0]
+    })
+}
