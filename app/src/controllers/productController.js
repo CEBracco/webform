b2bf352b85ab374ab86ea2baae6f91039fe45c5e
CEBracco <bracco66.cb@gmail.com>
2024-10-16 00:16:25 -0300

    fix products order

--- app/src/controllers/productController.js
+++ app/src/controllers/productController.js
@@ -9,16 +9,20 @@ global.server.app.get(['/:orderId?', '/index.html/:orderId?'], function (req, re
         if (!orderId) {
             orderId = 'default'
         }
-        db.Product.findAll({ 
-            where: { enabled: true }, 
-            include: [db.Group], 
-            order: [[db.Group, 'order'], ['price', 'name']] 
-        }).then(products => {
-            db.Order.findOne({ where: { hash: orderId } }).then(order => {
-                res.render("product", { products: products, order: order, priceFunction: OrderUtils.getProductPrice });
-                res.end();
+        db.Product.findAll({
+          where: { enabled: true },
+          include: [db.Group],
+          order: [[db.Group, "order"], ["price"], ["name"]],
+        }).then((products) => {
+          db.Order.findOne({ where: { hash: orderId } }).then((order) => {
+            res.render("product", {
+              products: products,
+              order: order,
+              priceFunction: OrderUtils.getProductPrice,
             });
-        })
+            res.end();
+          });
+        });
     } else {
         disableSite(siteDisabledParams, res);
     }
